dd4462b7ab930573c09f9044e571e4ce7c56eb80
Nurunnahar-Ema <nurunnaharema14@gmail.com>
2026-07-15 07:46:59 +0600

    fgfggff

--- src/component/apiurl.jsx
+++ src/component/apiurl.jsx
@@ -5,7 +5,7 @@ const url = window.location.hostname === "127.0.0.1" ||
             window.location.hostname === "localhost" || 
             window.location.hostname === "192.168.0.103"
     ? "http://127.0.0.1:8787" 
-    : "https://flowspace.piipra.me/api";
+    : "https://flowspace.piipra.me";
 
 const api = axios.create({
   baseURL: url,
